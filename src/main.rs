5d2ed7b325fcd069f2b8904c3050d8f6be75955b
Mai <mai@tilde.team>
2025-05-12 01:56:10 -0300

    paddle moves

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,56 @@ use bevy::prelude::*;
 fn main() {
     let mut app = App::new();
 
-    let lil_block = Block {
-        pos: Vector2::new(10., 10.),
-        dim: Vector2::new(40., 16.),
-        colour: LinearRgba::new(255., 0., 0., 255.),
-    };
-
-    app.add_plugins(DefaultPlugins).add_systems(
-        Startup,
-        move |mut commands: Commands,
-              mut meshes: ResMut<Assets<Mesh>>,
-              mut materials: ResMut<Assets<ColorMaterial>>| {
-            commands.spawn(Camera2d);
-            commands.spawn((
-                Mesh2d(meshes.add(Rectangle::new(lil_block.dim.x, lil_block.dim.y))),
-                MeshMaterial2d(materials.add(Color::LinearRgba(lil_block.colour))),
-                Transform::from_xyz(lil_block.pos.x, lil_block.pos.y, 1.),
-            ));
-        },
-    );
+    app.add_plugins(DefaultPlugins).add_systems(Startup, setup);
+    app.add_systems(Update, handle_key);
 
     app.run();
 }
 
-#[derive(Component)]
-struct Block {
-    pub pos: Vector2,
-    pub dim: Vector2,
-    pub colour: LinearRgba,
+fn setup(mut commands: Commands) {
+    commands.spawn(Paddle::make_entity());
+    commands.spawn(Camera2d);
 }
 
-struct Vector2 {
-    pub x: f32,
-    pub y: f32,
+fn handle_key(
+    key: Res<ButtonInput<KeyCode>>,
+    mut paddle_transform: Single<&mut Transform, With<Paddle>>,
+) {
+    let shift: f32 = if key.pressed(KeyCode::KeyA) {
+        -Paddle::speed()
+    } else if key.pressed(KeyCode::KeyD) {
+        Paddle::speed()
+    } else {
+        return;
+    };
+
+    paddle_transform.translation.x += shift;
 }
 
-impl Vector2 {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self { x, y }
+#[derive(Component, Debug)]
+struct Paddle;
+
+impl Paddle {
+    pub fn speed() -> f32 {
+        1.
+    }
+    pub fn initial_pos() -> Vec2 {
+        Vec2::new(0., 0.)
+    }
+    pub fn dim() -> Vec2 {
+        Vec2::new(50., 6.)
+    }
+    pub fn sprite() -> Sprite {
+        Sprite::from_color(
+            Color::LinearRgba(LinearRgba::new(160., 10., 10., 255.)),
+            Self::dim(),
+        )
+    }
+    pub fn transform() -> Transform {
+        let pos = Self::initial_pos();
+        Transform::from_xyz(pos.x, pos.y, 1.)
+    }
+    pub fn make_entity() -> (Sprite, Transform, Self) {
+        (Self::sprite(), Self::transform(), Self)
     }
 }
